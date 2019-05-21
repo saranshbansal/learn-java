2d6474af38d4af7f1211c7519275facd588364f7
Saransh Bansal <saran3h.bansal@gmail.com>
2019-05-21 16:32:42 +0530

    more programs added.

--- src/com/scratchpad/Test123.java
+++ src/com/scratchpad/Test123.java
@@ -1,34 +1,36 @@
 package com.scratchpad;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Test123
 {
-    private String responseMessage;
-
-
-    public String getResponseMessage()
-    {
-        return responseMessage;
-    }
-
-
-    public void setResponseMessage(String responseMessage)
-    {
-        if (StringUtils.isNotBlank(responseMessage) && responseMessage.length() > 10)
-        {
-            responseMessage = responseMessage.substring(0, 10);
-        }
-        this.responseMessage = responseMessage;
-    }
-
-
     public static void main(String[] args)
     {
-        Test123 o = new Test123();
-        o.setResponseMessage(null);
-        System.out.println(o.getResponseMessage());
-        String s = "";
-        System.out.println(s.toUpperCase());
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] x = new int[n];
+        for (int x_i = 0; x_i < n; x_i++)
+        {
+            x[x_i] = in.nextInt();
+        }
+        Arrays.sort(x);
+        int count = 0;
+        int i = 0;
+        int loc;
+        while (i < n)
+        {
+            count++;
+            loc = x[i] + k;
+            while (i < n && loc >= x[i])
+                i++;
+            loc = x[--i] + k;
+            while (i < n && loc >= x[i])
+                i++;
+        }
+        System.out.println(count);
+
     }
 }
