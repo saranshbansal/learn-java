5479c39335c978e0bf8eea9c6c22849188350a61
Saransh Bansal <saran3h.bansal@gmail.com>
2024-01-11 18:32:07 +0530

    new program added

--- src/com/datastructures/tree/SortedArrayToBST.java
+++ src/com/datastructures/tree/SortedArrayToBST.java
@@ -2,30 +2,33 @@ package com.datastructures.tree;
 
 public class SortedArrayToBST {
 
+	/**
+	 * Given an array where elements are sorted in ascending order, convert it
+	 * to a height balanced BST.
+	 *
+	 * @param nums sorted array
+	 * @return BST
+	 */
 	public static TreeNode sortedArrayToBST(int[] nums) {
-		TreeNode tree = null;
-		for (int num : nums) {
-			tree = insertRec(tree, num);
-		}
-
-		printView(tree);
-		return tree;
+		if (nums.length == 0) return null;
+		return toBstRec(nums, 0, nums.length - 1);
 	}
 
-	public static TreeNode insertRec(TreeNode root, int val) {
-		if (root == null) {
-			root = new TreeNode(val);
-		}
-
-		if (val > root.key) {
-			root.right = insertRec(root.right, val);
-		}
-
-		if (val < root.key) {
-			root.left = insertRec(root.left, val);
-		}
-
-		return root;
+	/**
+	 * Recursive method to transform sorted array to BST.
+	 *
+	 * @param nums sorted array
+	 * @param l left index
+	 * @param h right index
+	 * @return partial BST
+	 */
+	private static TreeNode toBstRec(int[] nums, int l, int h) {
+		if (l > h) return null;
+		int mid = (l + h) / 2;
+		TreeNode node = new TreeNode(nums[mid]);
+		node.left = toBstRec(nums, l, mid - 1);
+		node.right = toBstRec(nums, mid + 1, h);
+		return node;
 	}
 
 	private static void printView(TreeNode tree) {
@@ -37,8 +40,9 @@ public class SortedArrayToBST {
 	}
 
 	public static void main(String[] args) {
-		int[] nums = new int[]{-10,-3,0,5,9}; // Input array
+		int[] nums = new int[]{-10, -3, 0, 5, 9}; // Input array
 
-		sortedArrayToBST(nums);
+		TreeNode bstTree = sortedArrayToBST(nums); // Transform array to BST
+		printView(bstTree); // Print the sorted array
 	}
 }
---
+++ src/com/scratchpad/MapToString.java
@@ -0,0 +1,22 @@
+package com.scratchpad;
+
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.apache.commons.lang3.StringUtils.strip;
+
+public class MapToString {
+	public static void main(String[] args) {
+		Map<String, Object> someMap = new HashMap<>();
+		someMap.put("key", "value");
+		someMap.put("key2", "value2");
+		someMap.put("key3", null);
+		someMap.values().removeIf(Objects::isNull);
+		String output = StringUtils.join(someMap);
+		System.out.println(strip(output, "[]"));
+	}
+}
